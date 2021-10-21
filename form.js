73527ca5a2b6d2f1c0e252f620ea836e3b6a79e9
Murtaza M <murtazamister1@gmail.com>
2021-10-21 11:28:02 +0530

    EduDone

--- form.js
+++ form.js
@@ -1,11 +1,13 @@
-function makeVisible() {
+// Work Experience
+
+function wmakeVisible() {
   $("#accordionWork .accordion-header").css("display", "block");
 }
 
 function delWork(event) {
   event.preventDefault();
   if ($("#accordionWork .accordion-item").length > 1) {
-    makeVisible();
+    wmakeVisible();
     event.target.parentElement.parentElement.remove();
   }
   event.stopPropagation();
@@ -14,13 +16,40 @@ function delWork(event) {
 function delWork2(event) {
   event.preventDefault();
   if ($("#accordionWork .accordion-item").length > 1) {
-    makeVisible();
+    wmakeVisible();
+    event.target.parentElement.parentElement.parentElement.remove();
+  }
+  event.stopPropagation();
+}
+
+// Education and Qualifications
+
+function emakeVisible() {
+  $("#accordionEdu .accordion-header").css("display", "block");
+}
+
+function delEdu(event) {
+  event.preventDefault();
+  if ($("#accordionEdu .accordion-item").length > 1) {
+    emakeVisible();
+    event.target.parentElement.parentElement.remove();
+  }
+  event.stopPropagation();
+}
+
+function delEdu2(event) {
+  event.preventDefault();
+  if ($("#accordionEdu .accordion-item").length > 1) {
+    emakeVisible();
     event.target.parentElement.parentElement.parentElement.remove();
   }
   event.stopPropagation();
 }
 
 $(document).ready(function () {
+
+  // Work Experience
+
   let workAdder = $("#accordionWork").html();
   let workCounter = 1;
 
@@ -29,55 +58,68 @@ $(document).ready(function () {
     if ($("#accordionWork .accordion-item").length > 0) {
       $("#accordionWork .accordion-header").css("display", "block");
       let count = $("#accordionWork .accordion-item").length;
-      if (
-        document
-          .getElementById("accordionWork")
-          .getElementsByClassName("accordion-item")
-          [count - 1].getElementsByClassName("accordion-collapse")[0]
-          .classList.contains("show")
-      ) {
-        document
-          .getElementById("accordionWork")
-          .getElementsByClassName("accordion-item")
-          [count - 1].getElementsByClassName("accordion-button")[0]
-          .click();
+      if (document.getElementById("accordionWork").getElementsByClassName("accordion-item")[count - 1].getElementsByClassName("accordion-collapse")[0].classList.contains("show")){
+        document.getElementById("accordionWork").getElementsByClassName("accordion-item")[count - 1].getElementsByClassName("accordion-button")[0].click();
       }
     }
     $("#accordionWork").append(workAdder);
 
-    $("#accordionWork .accordion-header")
-      .last()
-      .attr("id", "heading" + workCounter);
-    $("#accordionWork .accordion-collapse")
-      .last()
-      .attr("aria-labelledby", "heading" + workCounter);
-    $("#accordionWork .accordion-collapse")
-      .last()
-      .attr("id", "collapse" + workCounter);
-    $("#accordionWork .accordion-button")
-      .last()
-      .attr("data-bs-target", "#collapse" + workCounter);
-    $("#accordionWork .accordion-button")
-      .last()
-      .attr("aria-controls", "collapse" + workCounter);
+    $("#accordionWork .accordion-header").last().attr("id", "wheading" + workCounter);
+    $("#accordionWork .accordion-collapse").last().attr("aria-labelledby", "wheading" + workCounter);
+    $("#accordionWork .accordion-collapse").last().attr("id", "wcollapse" + workCounter);
+    $("#accordionWork .accordion-button").last().attr("data-bs-target", "#wcollapse" + workCounter);
+    $("#accordionWork .accordion-button").last().attr("aria-controls", "wcollapse" + workCounter);
   });
 
   $(".fc2").mouseleave(function () {
-    makeVisible();
+    wmakeVisible();
     let count = $("#accordionWork .accordion-item").length;
     for (let i = 0; i < count; i++) {
-      if (
-        document
-          .getElementById("accordionWork")
-          .getElementsByClassName("accordion-item")
-          [i].getElementsByClassName("accordion-collapse")[0]
-          .classList.contains("show")
-      ) {
-        document
-          .getElementById("accordionWork")
-          .getElementsByClassName("accordion-button")
-          [i].click();
+      if (document.getElementById("accordionWork").getElementsByClassName("accordion-item")[i].getElementsByClassName("accordion-collapse")[0].classList.contains("show")){
+        document.getElementById("accordionWork").getElementsByClassName("accordion-button")[i].click();
       }
     }
   });
+
+
+  // Education and Qualifications
+
+
+  let eduAdder = $("#accordionEdu").html();
+  let eduCounter = 1;
+
+  $("#add_edu").click(function () {
+    eduCounter++;
+    if ($("#accordionEdu .accordion-item").length > 0) {
+      $("#accordionEdu .accordion-header").css("display", "block");
+      let count = $("#accordionEdu .accordion-item").length;
+      if (document.getElementById("accordionEdu").getElementsByClassName("accordion-item")[count - 1].getElementsByClassName("accordion-collapse")[0].classList.contains("show")){
+        document.getElementById("accordionEdu").getElementsByClassName("accordion-item")[count - 1].getElementsByClassName("accordion-button")[0].click();
+      }
+    }
+    $("#accordionEdu").append(eduAdder);
+
+    $("#accordionEdu .accordion-header").last().attr("id", "eheading" + eduCounter);
+    $("#accordionEdu .accordion-collapse").last().attr("aria-labelledby", "eheading" + eduCounter);
+    $("#accordionEdu .accordion-collapse").last().attr("id", "ecollapse" + eduCounter);
+    $("#accordionEdu .accordion-button").last().attr("data-bs-target", "#ecollapse" + eduCounter);
+    $("#accordionEdu .accordion-button").last().attr("aria-controls", "ecollapse" + eduCounter);
+  });
+
+  $(".fc1").mouseleave(function () {
+    emakeVisible();
+    let count = $("#accordionEdu .accordion-item").length;
+    for (let i = 0; i < count; i++) {
+      if (document.getElementById("accordionEdu").getElementsByClassName("accordion-item")[i].getElementsByClassName("accordion-collapse")[0].classList.contains("show")){
+        document.getElementById("accordionEdu").getElementsByClassName("accordion-button")[i].click();
+      }
+    }
+  });
+
+
+
+
+
+
+
 });
