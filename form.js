951ece05e2d1777ddf001d2b659941897b4242b4
Murtaza M <murtazamister1@gmail.com>
2021-11-05 12:42:53 +0530

    CSC done

--- form.js
+++ form.js
@@ -353,3 +353,83 @@ $(".fc4").mouseleave(function () {
   // if ($('#form1').find('#fname').val().trim() == '' || $('#form1').find('#lname').val().trim() == '' || $('#form1').find('#email').val().trim() == '' || $('#form1').find('#cnumber').val().trim() == '' || $('#form1').find('#address').val().trim() == '' || $('#form1').find('#city').val().trim() == '' || $('#form1').find('#state').val().trim() == '' || $('#form1').find('#zip').val().trim() == '' || $('#form1').find('#gender').val().trim() == '' || $('#form1').find('#bdate').val().trim() == '') {
 
   // }
+
+
+  let auth_token;
+  $(document).ready(function(){
+      $.ajax({
+          type: 'get',
+          url: 'https://www.universal-tutorial.com/api/getaccesstoken',
+          success: function(data){
+              auth_token = data.auth_token;
+              getCountries();
+          },
+          error: function(error){
+              console.log(error);
+          },
+          headers: {
+              "Accept": "application/json",
+              "api-token": "QFZCxL-P9DDVZzxIYTti85dbkTb-RZYqW4fG39dTvmeLJ9TCRmVj-UQSruPENKH3MCw",
+              "user-email": "murtazamister1@gmail.com"
+          }
+      })
+  })
+  function getCountries(){
+      $.ajax({
+          type: 'get',
+          url: 'https://www.universal-tutorial.com/api/countries',
+          success: function(data){
+            $('#country').empty();
+              data.forEach((ele)=>{
+                $('#country').append(`<option value="${ele.country_name}">${ele.country_name}</option>`);
+              })
+              // getStates();
+          },
+          error: function(error){
+              console.log(error);
+          },
+          headers: {
+              "Authorization": "Bearer "+auth_token,
+              "Accept": "application/json"
+          }
+      })
+  }
+  function getStates(){
+      $.ajax({
+          type: 'get',
+          url: 'https://www.universal-tutorial.com/api/states/'+$('#country').val(),
+          success: function(data){
+            $('#state').empty();
+            data.forEach((ele)=>{
+              $('#state').append(`<option value="${ele.state_name}">${ele.state_name}</option>`);
+            })
+            // getCities();
+          },
+          error: function(error){
+              console.log(error);
+          },
+          headers: {
+              "Authorization": "Bearer "+auth_token,
+              "Accept": "application/json"
+          }
+      })
+  }
+  function getCities(){
+      $.ajax({
+          type: 'get',
+          url: 'https://www.universal-tutorial.com/api/cities/'+$('#state').val(),
+          success: function(data){
+            $('#city').empty();
+            data.forEach((ele)=>{
+              $('#city').append(`<option value="${ele.city_name}">${ele.city_name}</option>`);
+            })
+          },
+          error: function(error){
+              console.log(error);
+          },
+          headers: {
+              "Authorization": "Bearer "+auth_token,
+              "Accept": "application/json"
+          }
+      })
+  }
