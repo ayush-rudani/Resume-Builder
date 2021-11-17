96a0b4f6c7d7f33b29a2a0e1bbeb1b1a495fc348
Murtaza M <murtazamister1@gmail.com>
2021-11-17 12:22:20 +0530

    Template2 set

--- Templates/template2.js
+++ Templates/template2.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-    console.log("hi");
-    $('.two').css("border","3px solid white");
-    $('.pelement').click(function(){
-       $('.pelement').css("border","3px solid transparent");
+    $('.t2 .two').css("border","3px solid white");
+    $('.t2 .pelement').click(function(){
+       $('.t2 .pelement').css("border","3px solid transparent");
        $(this).css("border","3px solid white");
-       $('.upper').css("background-color",$(this).css("background-color"));
+       $('.t2 .upper').css("background-color",$(this).css("background-color"));
     })
 })
--- form.js
+++ form.js
@@ -628,12 +628,5 @@ $(document).ready(function () {
   })
 })
 
-// ********************************* Template 2 ****************************************
-$(document).ready(function(){
-  $('.t2 .two').css("border","3px solid white");
-  $('.t2 .pelement').click(function(){
-     $('.t2 .pelement').css("border","3px solid transparent");
-     $(this).css("border","3px solid white");
-     $('.t2 .upper').css("background-color",$(this).css("background-color"));
-  })
-})
+
+// ********************************* Template 2's js has been added using script src ****************************************
