88e04d8cac8a55a984d15209ba384ac62a58f5ae
Ayush <ayushcrudani@gmail.com>
2021-11-20 00:25:49 +0530

    Final Update

--- Templates/template1.js
+++ Templates/template1.js
@@ -1,22 +1,22 @@
 function addStr(str, index, stringToAdd) {
     return str.substring(0, index) + stringToAdd + str.substring(index, str.length);
-  }
-  
-  function appender(z, value) {
+}
+
+function appender(z, value) {
     return z.substring(0, z.length - 1) + value + z.substring(z.length - 1);
-  }
-  
-  window.onload = function () {
+}
+
+window.onload = function () {
     document.getElementByClassName('t1')[0].getElementsByClassName("one")[0].click();
-  };
-  
-  $(document).ready(function () {
+};
+
+$(document).ready(function () {
     $('.t1 .one').css("border", "3px solid white");
     $('.t1 .pelement').click(function () {
-      $('.t1 .pelement').css("border", "3px solid transparent");
-      $(this).css("border", "3px solid white");
-      $('.t1 .left_side').css("background-color", $(this).css("background-color"));
-      $('.t1 .right_side .about .text h4').css("color", appender(addStr($(this).css("background-color"), 3, "a"), ", 0.82"));
-      $('.t1 .right_side .percent div').css("background-color", appender(addStr($(this).css("background-color"), 3, "a"), ", 0.65"));
+        $('.t1 .pelement').css("border", "3px solid transparent");
+        $(this).css("border", "3px solid white");
+        $('.t1 .left_side').css("background-color", $(this).css("background-color"));
+        $('.t1 .right_side .about .text h4').css("color", appender(addStr($(this).css("background-color"), 3, "a"), ", 0.82"));
+        $('.t1 .right_side .percent div').css("background-color", appender(addStr($(this).css("background-color"), 3, "a"), ", 0.65"));
     })
-  })
+})
--- Templates/template2.js
+++ Templates/template2.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $('.t2 .two').css("border","3px solid white");
-    $('.t2 .pelement').click(function(){
-       $('.t2 .pelement').css("border","3px solid transparent");
-       $(this).css("border","3px solid white");
-       $('.t2 .upper').css("background-color",$(this).css("background-color"));
+$(document).ready(function () {
+    $('.t2 .two').css("border", "3px solid white");
+    $('.t2 .pelement').click(function () {
+        $('.t2 .pelement').css("border", "3px solid transparent");
+        $(this).css("border", "3px solid white");
+        $('.t2 .upper').css("background-color", $(this).css("background-color"));
     })
 })
